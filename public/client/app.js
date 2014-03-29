00e9f080e261546ff8a36abcf5231f2681a7568e
Hack Reactor Students <accounts@hackreactor.com>
2014-03-29 11:53:29 -0700

    enable create link functionality

--- public/client/app.js
+++ public/client/app.js
@@ -1,42 +1,40 @@
-// require ngRoute
-var app = angular.module('myApp', []);
+var app = angular.module('myApp', ['ngRoute']);
 
-// app.service('Links', function($http) {
-//   return {
-//     getLinks: function(){
-//       var promise = $http({
-//         method: 'GET',
-//         url: '/links'
-//       }).then(function(data){
-//         return data;
-//       });
-//       return promise;
-//     }
-//   };
-// });
+app.service('get_Links', function($http) {
+  return {
+    getLinks: function(){
+      var promise = $http({
+        method: 'GET',
+        url: '/links'
+      }).then(function(data){
+        return data;
+      });
+      return promise;
+    }
+  };
+});
 
-// app.run(function($rootScope) {
-//   $rootScope.name = 'someone else';
-// });
+app.run(function($rootScope) {
+  $rootScope.name = 'someone else';
+});
 
-// app.controller('linkController', function($scope, Links) {
-//   Links.getLinks().then(function(data){
-//     $scope.links = data.data;
-//     console.log('WAAAAAAAAAAAAAAAAAHHHHHHHHHHH', data);
-//   });
-// });
-
-// app router
-// app.config(function($routeProvider, $locationProvider){
-//   $routeProvider
-//     .when('')
-// });
-
-app.controller('linkController', function($scope, $http){
-  $http({
-    method: 'GET',
-    url: '/links'
-  }).then(function(links){
-    $scope.links = links.data;
+app.controller('linkController', function($scope, $http, get_Links) {
+  get_Links.getLinks().then(function(data){
+    $scope.links = data.data;
   });
+  $scope.addLink = function() {
+    $http({
+      method: 'POST',
+      url: '/links',
+      data: {url: $scope.linkText}
+    });
+  };
+});
+
+// app router*******************************************************
+app.config(function($routeProvider, $locationProvider){
+  $routeProvider
+    .when('/create', {
+      templateUrl: 'signup.ejs'
+    });
 });
